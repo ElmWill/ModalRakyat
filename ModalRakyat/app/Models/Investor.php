db3bd940ae4001ce83d4268983908054f605a624
Davidkys <david.arifin@binus.ac.id>
2025-06-01 21:28:00 +0700

    Set Up Investor Database Attributes

---
+++ ModalRakyat/app/Models/Investor.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Investor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'investors';
+    protected $primaryKey = 'investorID';
+    public $incrementing = true;
+    protected $fillable = [
+        'userID',
+        'bankAccountNumber',
+        'bankName',
+        'accountHolderName',
+        'identityCardNumber',
+        'identityCardScanURL',
+        'taxIDNumber',
+        'investmentPreferences',
+        'riskProfile',
+        'totalInvestedAmount',
+        'currentInvestmentValue',
+        'availableBalance',
+    ];
+    protected $casts = [
+        'investmentPreferences' => 'array',
+        'totalInvestedAmount' => 'decimal:2',
+        'currentInvestmentValue' => 'decimal:2',
+        'availableBalance' => 'decimal:2',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userID', 'userID');
+    }
+}
