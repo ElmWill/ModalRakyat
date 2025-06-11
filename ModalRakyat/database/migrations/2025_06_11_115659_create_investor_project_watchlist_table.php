7dc56a5d3c18093f19f231e43e5367af679340c4
Davidkys <david.arifin@binus.ac.id>
2025-06-11 19:25:31 +0700

    Create table ManyToMany of watchlist that contain project and investor

---
+++ ModalRakyat/database/migrations/2025_06_11_115659_create_investor_project_watchlist_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('investor_project_watchlist', function (Blueprint $table) {
+            $table->foreignId('investorID')
+                  ->constrained('investors', 'investorID') 
+                  ->onDelete('cascade');
+
+            $table->foreignId('projectID')
+                  ->constrained('umkm_projects', 'projectID') 
+                  ->onDelete('cascade');
+
+            $table->primary(['investorID', 'projectID']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('investor_project_watchlist');
+    }
+};
