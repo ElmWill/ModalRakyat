6daa77c713bdac0899999b8b3efca6f2cebe5c5d
Brian Chang <brianchang@Brians-MacBook-Air.local>
2025-06-14 16:31:28 +0700

    add another umkm

--- ModalRakyat/resources/views/Page/UMKM.jsx
+++ ModalRakyat/resources/views/Page/UMKM.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
 import Footer from '../Component/Footer/Footer'
 import FavoriteUMKM from '../Component/UMKM/FavoriteUMKM/FavoriteUMKM'
+import AnotherUMKM from '../Component/UMKM/AnotherUMKM/AnotherUMKM'
 
 const UMKM = () => {
   return (
     <div>
         <Navbar/>
         <FavoriteUMKM/>
+        <AnotherUMKM/>
         <Footer/>
     </div>
   )
