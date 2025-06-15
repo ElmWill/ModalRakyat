a2bfd4a506f19f5dcd99d14968b71e43612b6923
Brian Chang <brianchang@Brians-MacBook-Air.local>
2025-06-15 19:44:54 +0700

    my-wallet page

--- ModalRakyat/resources/views/Page/MyWallet/MyWalletUMKM.jsx
+++ ModalRakyat/resources/views/Page/MyWallet/MyWalletUMKM.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MyWalletUMKM.css'
+import modalrakyat_logo from '../../assets/modal rakyat_warna2.png'
+import Deposit from '../../Component/MyWallet/Deposit'
+import Withdraw from '../../Component/MyWallet/Withdraw'
+import Balance from '../../Component/Balance/Balance'
+import History from '../../Component/MyWallet/History'
 
 const MyWalletUMKM = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [balance, setBalance] = useState(20000000) // saldo awal Rp.20.000.000
+    // const [historyData, setHistoryData] = useState([]) // riwayat transaksi
+    const [historyData, setHistoryData] = useState([ // data dummy
+    { type: 'DEPOSIT', status: 'SUCCESSED', amount: 150000 },
+    { type: 'WITHDRAW', status: 'FAILED', amount: 50000 },
+    { type: 'DEPOSIT', status: 'SUCCESSED', amount: 300000 }
+    ])
+
+    return (
+        <div className='my-wallet'>
+            <div id='modal-rakyat-logo'>
+                <img src={modalrakyat_logo} alt="" />
+            </div>
+            <div id="depo-with">
+                <Deposit 
+                setBalance={setBalance} 
+                setHistoryData={setHistoryData} 
+                />
+                <Withdraw 
+                balance={balance} 
+                setBalance={setBalance} 
+                setHistoryData={setHistoryData} 
+                />
+            </div>
+            <Balance balance={balance} />
+            <History historyData={historyData} />
+        </div>
+    )
 }
 
 export default MyWalletUMKM
