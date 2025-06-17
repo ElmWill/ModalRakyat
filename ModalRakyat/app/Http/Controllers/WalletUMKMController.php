c1bb7648ba9107282858491e66a4dbeca2b9e67c
Davidkys <david.arifin@binus.ac.id>
2025-06-18 03:03:00 +0700

    BE:Adding investor investment features so investor can buy umkm projects

--- ModalRakyat/app/Http/Controllers/WalletUMKMController.php
+++ ModalRakyat/app/Http/Controllers/WalletUMKMController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WalletController extends Controller
+class WalletUMKMController extends Controller
 {
     public function tracking(Request $request)
     {
--- ModalRakyat/routes/api.php
+++ ModalRakyat/routes/api.php
@@ -16,38 +16,45 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvestorController;
 use App\Models\Role;
 
-Route::prefix('investor')->name('investor.')->group(function () {
-    Route::get('/dashboard', [InvestorController::class, 'index'])->name('dashboard');
-});
-
-Route::prefix('watchlist')->name('watchlist.')->group(function () {
-    Route::get('/', [WatchlistController::class, 'index'])->name('index');
-    Route::post('/{project:projectID}', [WatchlistController::class, 'store'])->name('add');
-    Route::delete('/{project:projectID}', [WatchlistController::class, 'destroy'])->name('remove');
-});
-
-Route::prefix('favorites')->name('favorites.')->group(function () {
-    Route::get('/', [FavoriteController::class, 'index'])->name('index');
-    Route::post('/{project:projectID}', [FavoriteController::class, 'store'])->name('add');
-    Route::delete('/{project:projectID}', [FavoriteController::class, 'destroy'])->name('remove');
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/projects', [SearchController::class, 'searchProjects'])->name('projects.search');
 Route::get('/projects/{project:projectID}', [UMKMProjectController::class, 'show'])->name('projects.show');
 
-Route::prefix('wallet')->name('wallet.')->group(function () {
-    Route::post('/deposit', [WalletController::class, 'prepareDeposit'])->name('deposit.prepare');
-    Route::post('/deposit/{transaction:transactionID}/confirm', [WalletController::class, 'confirmDeposit'])->name('deposit.confirm');
-    Route::post('/withdrawal', [WalletController::class, 'requestWithdrawal'])->name('withdrawal.request');
-});
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user/profile', [AuthController::class, 'profile']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/switch-role', [AuthController::class, 'switchRole']);
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+
+    Route::post('/logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out']);
+    });
+
+    Route::prefix('investor')->name('investor.')->group(function () {
+        Route::get('/dashboard', [InvestorController::class, 'index'])->name('dashboard');
+    });
+
+    Route::prefix('watchlist')->name('watchlist.')->group(function () {
+        Route::get('/', [WatchlistController::class, 'index'])->name('index');
+        Route::post('/{project:projectID}', [WatchlistController::class, 'store'])->name('add');
+        Route::delete('/{project:projectID}', [WatchlistController::class, 'destroy'])->name('remove');
+    });
+
+    Route::prefix('favorites')->name('favorites.')->group(function () {
+        Route::get('/', [FavoriteController::class, 'index'])->name('index');
+        Route::post('/{project:projectID}', [FavoriteController::class, 'store'])->name('add');
+        Route::delete('/{project:projectID}', [FavoriteController::class, 'destroy'])->name('remove');
+    });
+
+    Route::prefix('wallet')->name('wallet.')->group(function () {
+        Route::post('/deposit', [WalletController::class, 'prepareDeposit'])->name('deposit.prepare');
+        Route::post('/deposit/{transaction:transactionID}/confirm', [WalletController::class, 'confirmDeposit'])->name('deposit.confirm');
+        Route::post('/withdrawal', [WalletController::class, 'requestWithdrawal'])->name('withdrawal.request');
+    });
+
+    Route::post('/projects/{project:projectID}/invest', [InvestorController::class, 'store'])->name('investments.store');
 });
 
 Route::middleware('auth:sanctum')->put('/user/update-profile', [AuthController::class, 'updateProfile']);
